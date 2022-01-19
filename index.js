b79391ecefb15e3198eb6cef4f5348997f7e0279
can-ishk <kchathley26@gmail.com>
2022-01-19 23:33:06 +0530

    Implemented add item(POST) and edit item(PUT) methods

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const fs = require('fs')
-
+const bodyParser = require('body-parser')
 const port = 3030;
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //Routes ahead
+
+//stock = []
+stock = [
+    {
+        "name": "ka",
+        "cost": "4",
+        "quantity": "3"
+    },
+    {
+        "name": "ab",
+        "cost": "3",
+        "quantity": "7"
+    },
+    {
+        "name": "cd",
+        "cost": "3",
+        "quantity": "6"
+    }
+]
 
 app.get('/',(req, res)=>{
     try{
@@ -18,10 +38,55 @@ app.get('/',(req, res)=>{
 
 app.get ('/shop', (req,res)=>{
     try{
-        fs.readFile(__dirname + '/store/' + 'stock.json', 'utf8', (err, data) => {
-            res.send(data);
-        });
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+res.write(JSON.stringify(stock));
+res.end();
     }catch(err){
+        console.log(err.message)
+    }
+})
+
+app.post('/shop/add', (req,res)=>{
+    try{
+        const item = req.body;
+        const search = stock.filter((i)=> {return i.name == item.name})
+        if(search.length>0){
+            res.status(406) //Not Acceptable
+            console.log()
+            res.end("Bad Request: Item already exists. Please use 'edit' instead of 'add'")
+        }
+        else{
+            stock.push(item)
+            res.send(stock);
+        }
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+app.put('/shop/edit', (req,res)=>{
+    try{
+        const item = req.body;
+        const search = stock.filter((i)=> {return i.name == item.currName})
+        if(search.length>0){
+            i = stock.findIndex(it=>it.name==item.currName)
+            if(item.name) stock[i].name = item.name 
+            if(item.cost) stock[i].cost = item.cost
+            if(item.quantity) stock[i].quantity = item.quantity
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(stock));
+            res.end();
+        }
+        else{
+            res.status(406) //Not Acceptable
+            console.log()
+            res.end("Bad Request: Item doesn't exist. Please use 'add' instead of 'edit'")
+
+        }
+
+    }
+    catch(err){
         console.log(err.message)
     }
 })
